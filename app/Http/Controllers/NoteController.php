379fa75adee4fac758910df3d4e374e9a5ea3d82
Shankara21 <lazuardit21@gmail.com>
2023-06-14 17:48:57 +0700

    initialize project

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use App\Http\Requests\StoreNoteRequest;
+use App\Http\Requests\UpdateNoteRequest;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $notes = Note::all();
+        return response($notes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNoteRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreNoteRequest $request)
+    {
+        $note = Note::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+        return response($note);
+    }
+
+    /*
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Note $note)
+    {
+        return response($note);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNoteRequest  $request
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNoteRequest $request, Note $note)
+    {
+        $note->update([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+        return response($note);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Note $note)
+    {
+        $note->delete();
+        return response('Note deleted successfully');
+    }
+}
